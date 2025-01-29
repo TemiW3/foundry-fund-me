a99b78cab6db6e95effa47e01c30a6aa55f8df45
Temi <temiw3@gmail.com>
2025-01-29 22:33:52 +0000

    mock price feeds

--- script/DeployFundme.s.sol
+++ script/DeployFundme.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundme is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeConfig();
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
